4e89b85817ea7a825521ca64453289588c11b779
Jeremy Antonoff <jeremyantonoff@gmail.com>
2017-10-04 22:59:21 -0400

    add contact route for contact form

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 
 
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + "/public"))
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", function(req, res) {
   res.render('home')
+});
+
+app.post("/contact", function(req, res){
+  const nodemailer = require('nodemailer');
+  let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+          user: process.env.mailerUSERNAME,
+          pass: process.env.mailerPASS
+      }
+  });
+
+  let mailOptions = {
+      from: req.body.name,
+      to: 'jeremyantonoff@gmail.com',
+      subject: 'New Message from Portfolio!',
+      html: '<h2>' + req.body.name + '<br></h2><h2>' + req.body.email + '<br></h2><p>' + req.body.message  + '</p>'
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.end('failure');
+    } else {
+      console.log('message-sent')
+      res.end('success');
+    }
+  });
 });
 
 app.listen(3000, function() {
